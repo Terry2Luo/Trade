213a8292cf2e056164b6914bcde96dcefed0542e
luxing <Luxing123456>
2021-01-27 09:39:39 +0800

    swagger格式风格调整

--- src/main/java/com/rongwei/pims/job/trade/domain/TradeInputVO.java
+++ src/main/java/com/rongwei/pims/job/trade/domain/TradeInputVO.java
@@ -1,5 +1,7 @@
 package com.rongwei.pims.job.trade.domain;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -14,6 +16,7 @@ import java.util.Date;
  * @date 2021-01-23 21:23:02
  */
 @Data
+@ApiModel(value="交易明细",description = "交易明细信息")
 public class TradeInputVO extends TradeInputDO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -30,18 +33,25 @@ public class TradeInputVO extends TradeInputDO implements Serializable {
 		this.sellType = sellType;
 	}
 	//交易主键
+	@ApiModelProperty(value="交易主键",example="1",required=true)
 	private Integer transcationId;
 	//交易员ID
+	@ApiModelProperty(value="交易员ID",example="1",required=true)
 	private Integer tradeId;
 	//版本号
+	@ApiModelProperty(value="版本号",example="1",required=true)
 	private Integer version;
 	//证券代码
+	@ApiModelProperty(value="证券代码",example="INF、REL、ITC",required=true)
 	private String securityCode;
 	//数量
+	@ApiModelProperty(value="数量",example="50、20",required=true)
 	private Integer quantity;
 	//操作类型
+	@ApiModelProperty(value="操作类型",example="INSERT、UPDATE、CANCEL",required=true)
 	private String operationType;
 	//买卖类型
+	@ApiModelProperty(value="买卖类型",example="Sell、Buy",required=true)
 	private String sellType;
 
 }
